3ea304537c81023b8432947d81b3771232774ead
linhbg1998 <52190759+linhbg1998@users.noreply.github.com>
2026-07-10 18:22:49 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * main.c - main file for using kairos, used to patch unpacked IM4P iOS bootloader images
  *
  * Copyright 2020 dayt0n
- * Modified: added CTRR lockdown and USB backdoor options.
+ * Modified: added CTRR, USB backdoor, image type bypass options.
  */
 
 #include "newpatch.h"
@@ -19,9 +19,10 @@ int main(int argc, char* argv[]) {
         printf("\t-n\t\t\tunlock nvram\n");
         printf("\t-ctrr\t\t\tNOP CTRR lockdown MSRs\n");
         printf("\t-usb\t\t\tinstall USB backdoor (custom commands)\n");
+        printf("\t-imgtype\t\tbypass image type checks\n");
         printf("\nExample usage:\n");
         printf("  %s iBEC.dec iBEC.patched -n -b \"-v debug=0x09\" -c \"go\" 0x830000300\n", argv[0]);
-        printf("  %s iBEC.dec iBEC.patched -ctrr -usb\n", argv[0]);
+        printf("  %s iBEC.dec iBEC.patched -ctrr -usb -imgtype\n", argv[0]);
         return -1;
     }
 
@@ -36,6 +37,7 @@ int main(int argc, char* argv[]) {
     bool doNvramUnlock = false;
     bool doCtrrLockdown = false;
     bool doUsbBackdoor = false;
+    bool doImageType = false;
 
     // Parse options
     for (int i = 3; i < argc; i++) {
@@ -50,6 +52,8 @@ int main(int argc, char* argv[]) {
             doCtrrLockdown = true;
         } else if (strcmp(argv[i], "-usb") == 0) {
             doUsbBackdoor = true;
+        } else if (strcmp(argv[i], "-imgtype") == 0) {
+            doImageType = true;
         } else {
             WARN("Unknown option: %s\n", argv[i]);
         }
@@ -111,6 +115,52 @@ int main(int argc, char* argv[]) {
             if (ret < 0)
                 WARN("Failed to unlock nvram\n");
         }
+    }
+
+    // 3. CTRR lockdown (always available)
+    if (doCtrrLockdown) {
+        LOG("Applying CTRR lockdown...\n");
+        ret = patch_ctrr_lockdown(&iboot_in);
+        if (ret < 0)
+            WARN("CTRR lockdown failed\n");
+    }
+
+    // 4. Image type bypass
+    if (doImageType) {
+        LOG("Patching image type checks...\n");
+        ret = patch_image_type(&iboot_in);
+        if (ret < 0)
+            WARN("Image type patch failed\n");
+    }
+
+    // 5. RSA signature check patch (always)
+    LOG("Patching out RSA signature check...\n");
+    ret = rsa_sigcheck_patch(&iboot_in, pac);
+    if (ret < 0)
+        WARN("Error patching out RSA signature check\n");
+
+    // 6. USB backdoor (install after all other patches)
+    if (doUsbBackdoor) {
+        LOG("Installing USB backdoor...\n");
+        ret = install_usb_backdoor(&iboot_in);
+        if (ret < 0)
+            WARN("USB backdoor installation failed\n");
+    }
+
+    // Write output
+    fp = fopen(outFile, "wb+");
+    if (!fp) {
+        printf("Error opening %s for writing\n", outFile);
+        free(iboot_in.buf);
+        return -1;
+    }
+    fwrite(iboot_in.buf, 1, iboot_in.len, fp);
+    fflush(fp);
+    fclose(fp);
+    free(iboot_in.buf);
+    LOG("Wrote patched image to %s\n", outFile);
+    return 0;
+}        }
     }
 
     // 3. CTRR lockdown (always available, no dependency)
